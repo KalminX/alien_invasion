e5639c89395c37a23ff5cbe9a664003fa9f21542
KalminX <noriabure@gmail.com>
2024-02-27 22:04:17 +0100

    added movements features

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,28 +1,26 @@
-import sys
 import pygame
 
 from settings import Settings
-
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     # initialize pygame, settings and screen object
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
-    screen = pygame.display.set_mode((600, 400))
+    screen = pygame.display.set_mode((900, 600))
     pygame.display.set_caption("Kalmin's Alien Invasion")
+    
+    # Make a ship
+    ship = Ship(ai_settings, screen)
  
 	# main game loop
     while True:
 		# loop through each event
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-                
-        # redraw the screen during each pass through the loop
-        screen.fill(ai_settings.bg_color)
-        # update the screen
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
