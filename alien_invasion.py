31b16058bab9062b6c145038c78b909973305664
KalminX <noriabure@gmail.com>
2024-02-28 03:24:24 +0100

    created a bullet class to store my bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -14,13 +15,17 @@ def run_game():
     
     # Make a ship
     ship = Ship(ai_settings, screen)
+    
+    # Make a group to store bullets in
+    bullets = Group()
  
 	# main game loop
     while True:
 		# loop through each event
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -8,19 +8,10 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # Move the ship to the right
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-                
+            check_keydown_events(event, ship)   
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
-                
-            
+            check_keyup_events(event, ship)
+
 def update_screen(ai_settings, screen, ship):
     """Update images on the screen and flip to the new screen"""
     # Redraw the screen during each pass through the loop
@@ -29,3 +20,19 @@ def update_screen(ai_settings, screen, ship):
     
     # Make the most recently drawn screen visible
     pygame.display.flip()
+    
+def check_keydown_events(event, ship):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+        # Move the ship to the right
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    
+def check_keyup_events(event, ship):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+    
