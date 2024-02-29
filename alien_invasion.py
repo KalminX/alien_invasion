71793538eb05f294e37b31eb35fc55d220adb502
KalminX <noriabure@gmail.com>
2024-02-29 17:14:49 +0100

    implemented game over and added a play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import pygame
 from pygame.sprite import Group
 
 from settings import Settings
+from game_stats import GameStats
+from button import Button
 from ship import Ship
 import game_functions as gf
 
@@ -10,22 +12,32 @@ def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
-    screen = pygame.display.set_mode((900, 600))
     pygame.display.set_caption("Kalmin's Alien Invasion")
+
+    # Make the Play button
+    play_button = Button(ai_settings, screen, "Play")
+
+    stats = GameStats(ai_settings)
     
     # Make a ship
     ship = Ship(ai_settings, screen)
     
-    # Make a group to store bullets in
+    # Make a group to store bullets
     bullets = Group()
+    aliens = Group()
+
+    # Create an alien fleet
+    gf.create_fleet(ai_settings, screen, ship, aliens)
  
 	# main game loop
     while True:
 		# loop through each event
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        bullets.update()
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 run_game()
--- settings.py
+++ settings.py
@@ -5,14 +5,21 @@ class Settings():
         """Initialize the game's settings."""
         # Screen settings
         self.screen_height = 600
-        self.screen_width = 400
+        self.screen_width = 900
         self.bg_color = (230, 230, 230)
         
         # Ship settings
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
+
+        # Alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
         
         # Bullet settings
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
