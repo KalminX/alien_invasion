e29d06448f6b3ff6cd56d80612dbaa167510b345
KalminX <noriabure@gmail.com>
2024-03-02 12:22:24 +0100

    implemented the high score functionality and the plane icons

--- game_stats.py
+++ game_stats.py
@@ -4,11 +4,15 @@ class GameStats():
         """Initialize statistics"""
         self.ai_settings = ai_settings
         self.reset_stats()
+        self.high_score = 0
         
         # Start Alien Invasion in an active state
         self.game_active = False
+
+        self.high_score = 0
     
     def reset_stats(self):
         """Initialize statistics that can change during the game"""
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0
+        self.level = 1
